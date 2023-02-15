028a3a17a0f2fcba6c5f001aa433c79c409c2af7
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2023-02-15 13:56:31 +0100

    Merge with Daelon's branch

--- addons/super_spector/inspector_plugin.gd
+++ addons/super_spector/inspector_plugin.gd
@@ -7,8 +7,12 @@ var plugin: EditorPlugin
 var checks = []
 var categories = {}
 var sections = {}
-
+var editor_selection_handled = true
+var selected_properties = {}
 # ******************************************************************************
+func editor_selection_changed():
+	editor_selection_handled = false
+
 
 func get_all_children(node, _children=[]):
 	for child in node.get_children():
@@ -28,7 +32,6 @@ func get_inspector_properties():
 func _can_handle(object) -> bool:
 	return true
 
-var selected_properties = {}
 
 func property_selected(value, source):
 	if value:
@@ -37,6 +40,10 @@ func property_selected(value, source):
 		selected_properties.erase(source.property_name)
 
 func _parse_end(object: Object) -> void:
+	if editor_selection_handled:
+		return
+	editor_selection_handled = true
+	
 	checks.clear()
 	categories.clear()
 	sections.clear()
@@ -55,13 +62,13 @@ func _parse_end(object: Object) -> void:
 			current_section = node
 			sections[current_section.name] = []
 
-		if ClassDB.get_parent_class(cls) == 'EditorProperty':			
+		if ClassDB.get_parent_class(cls) == 'EditorProperty':
 			property = node
 
-			var hbox = HBox.new()
+			var hbox = _InspectorHBox.new()
 			hbox.gui_input.connect(Callable(self._gui_input).bind(hbox))
 
-			var check = hbox.add(Check.new(node, current_category, current_section))
+			var check = hbox.add(_InspectorCheck.new(node, current_category, current_section))
 
 			if check.category:
 				categories[check.category.name].append(check)
@@ -96,57 +103,67 @@ func mouse_entered(source):
 			source.set_pressed(target_state)
 
 func _gui_input(event, source):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if source is CheckBox:
-				if event.pressed:
-					dragging = true
-					click_source = source
-					target_state = !click_source.button_pressed
-					click_source.set_pressed_no_signal(!click_source.button_pressed)
-				if !event.pressed:
-					if dragging:
-						if !dragged:
-							click_source.button_pressed = !click_source.button_pressed
-						dragged = false
-					dragging = false
+	if !(event is InputEventMouseButton):
+		return
+	if event.button_index == MOUSE_BUTTON_LEFT:
+		if source is CheckBox:
+			if event.pressed:
+				dragging = true
+				click_source = source
+				target_state = !click_source.button_pressed
+				click_source.set_pressed_no_signal(!click_source.button_pressed)
+			if !event.pressed:
+				if dragging:
+					if !dragged:
+						click_source.button_pressed = !click_source.button_pressed
+					dragged = false
+				dragging = false
 
-		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
-			if is_instance_valid(ctx):
-				ctx.queue_free()
+	if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+		if is_instance_valid(ctx):
+			ctx.queue_free()
 
-			ctx = ContextMenu.new(source, self.item_selected)
+		ctx = _InspectorContextMenu.new(source, self.item_selected)
 
-			var root = plugin.get_editor_interface().get_base_control()
-			var icon
+		var root = plugin.get_editor_interface().get_base_control()
+		var icon
+		icon = root.get_theme_icon('ActionCopy', 'EditorIcons')
+		ctx.add_icon_item(icon, 'Copy Selected Properties')
+		icon = root.get_theme_icon('ActionPaste', 'EditorIcons')
+		ctx.add_icon_item(icon, 'Paste Selected Properties')
+		icon = root.get_theme_icon('Duplicate', 'EditorIcons')
+		ctx.add_icon_item(icon, 'Copy With Unique Resources')
 
-			icon = root.get_theme_icon('ActionCopy', 'EditorIcons')
-			ctx.add_icon_item(icon, 'Copy Selected Properties')
-			icon = root.get_theme_icon('ActionPaste', 'EditorIcons')
-			ctx.add_icon_item(icon, 'Paste Selected Properties')
+		ctx.add_separator('Copy to Clipboard')
+		icon = root.get_theme_icon('CopyNodePath', 'EditorIcons')
+		ctx.add_icon_item(icon, 'Copy Property Paths')
+		ctx.add_icon_item(icon, 'Copy Properties as Code')
+		ctx.add_icon_item(icon, 'Copy Properties as Dictionary')
+		ctx.add_icon_item(icon, 'Copy Selected Properties in PropertyList format')
 
-			ctx.add_separator()
-			icon = root.get_theme_icon('CopyNodePath', 'EditorIcons')
-			ctx.add_icon_item(icon, 'Copy Selected Property Paths to Clipboard')
-			icon = root.get_theme_icon('CopyNodePath', 'EditorIcons')
-			ctx.add_icon_item(icon, 'Copy Selected Properties to Clipboard')
-			icon = root.get_theme_icon('CopyNodePath', 'EditorIcons')
-			ctx.add_icon_item(icon, 'Copy Selected Properties in PropertyList format')
-
-			ctx.add_separator()
+		ctx.add_separator()
+		var test = func(x): return x.button_pressed
+		if !checks.all(test):
 			ctx.add_item('Select All')
-			
-			if source.category:
-				ctx.add_item('Select All in Category')
-			if source.section:
-				ctx.add_item('Select All in Section')
+		if checks.any(test):
 			icon = root.get_theme_icon('Clear', 'EditorIcons')
-			ctx.add_icon_item(icon, 'Clear Selected Properties')
+			ctx.add_icon_item(icon, 'Deselect All')
+		
+		if source.category:
+			if !categories[source.category.name].all(test):
+				ctx.add_item('Select Entire Category')
+			if categories[source.category.name].any(test):
+				ctx.add_item('Deselect Entire Category')
+		if source.section:
+			if !sections[source.section.name].all(test):
+				ctx.add_item('Select Entire Section')
+			if sections[source.section.name].any(test):
+				ctx.add_item('Deselect Entire Section')
+		
+		var pos = root.get_global_mouse_position()
+		pos += root.get_screen_position()
+		ctx.open(pos)
 
-			var pos = root.get_global_mouse_position()
-			pos += root.get_screen_position()
-			ctx.open(pos)
-	
 func get_selected_items():
 	var selection = []
 	for check in checks:
@@ -191,17 +208,30 @@ func item_selected(item):
 		'Paste Selected Properties':
 			if copied_data:
 				paste_data()
-		'Copy Selected Property Paths to Clipboard':
+		'Copy With Unique Resources':
+			copied_data = get_selected_data()
+			for name in copied_data.keys():
+				if typeof(copied_data[name]) == TYPE_OBJECT:
+					copied_data[name] = copied_data[name].duplicate()
+		'Copy Property Paths':
 			var out = ''
 			for name in get_selected_data():
 				out += '%s\n' % [name]
 			DisplayServer.clipboard_set(out)
-		'Copy Selected Properties to Clipboard':
+		'Copy Properties as Code':
 			var out = ''
 			var data = get_selected_data()
 			for name in data:
 				out += '%s = %s\n' % [name, var_to_str(data[name])]
 			DisplayServer.clipboard_set(out)
+		'Copy Properties as Dictionary':
+			var data = get_selected_data()
+			var out = {}
+			for name in data:
+				if data[name] is bool or data[name] is float or data[name] is int or data[name] is String or data[name] == null:
+					out[name] = data[name]
+				else: out[name] = var_to_str(data[name])
+			DisplayServer.clipboard_set(JSON.stringify(out, '\t'))
 		'Copy Selected Properties in PropertyList format':
 			var out = ''
 			var data = get_selected_data()
@@ -213,21 +243,21 @@ func item_selected(item):
 			},' % [name, typeof(data[name])]
 			DisplayServer.clipboard_set(out)
 		'Select All':
-			for check in checks:
-				check.button_pressed = true
-		'Select All in Category':
-			for check in categories[source.category.name]:
-				check.button_pressed = true
-		'Select All in Section':
-			for check in sections[source.section.name]:
-				check.button_pressed = true
-		'Clear Selected Properties':
-			for check in checks:
-				check.button_pressed = false
+			checks.map(func(c): c.button_pressed = true)
+		'Deselect All':
+			checks.map(func(c): c.button_pressed = false)
+		'Select Entire Category':
+			categories[source.category.name].map(func(c): c.button_pressed = true)
+		'Deselect Entire Category':
+			categories[source.category.name].map(func(c): c.button_pressed = false)
+		'Select Entire Section':
+			sections[source.section.name].map(func(c): c.button_pressed = true)
+		'Deselect Entire Section':
+			sections[source.section.name].map(func(c): c.button_pressed = false)
 
 # ******************************************************************************
 
-class Check:
+class _InspectorCheck:
 	extends CheckBox
 
 	var property_name = ''
@@ -243,7 +273,7 @@ class Check:
 
 		tooltip_text = 'Select this property for multi-copying.'
 
-class HBox:
+class _InspectorHBox:
 	extends HBoxContainer
 
 	func _init() -> void:
@@ -255,7 +285,7 @@ class HBox:
 
 		return object
 
-class ContextMenu:
+class _InspectorContextMenu:
 	extends PopupMenu
 
 	signal item_selected(item)
@@ -263,8 +293,7 @@ class ContextMenu:
 	func _init(obj=null, cb=null):
 		if obj:
 			obj.add_child(self)
-
-		if obj and cb:
+		if cb:
 			item_selected.connect(cb)
 
 		index_pressed.connect(self._on_index_pressed)
--- addons/super_spector/plugin.gd
+++ addons/super_spector/plugin.gd
@@ -8,6 +8,9 @@ var inspector_plugin
 func _enter_tree():
 	inspector_plugin = preload('inspector_plugin.gd').new()
 	inspector_plugin.plugin = self
+	
+	var selection = get_editor_interface().get_selection()
+	selection.selection_changed.connect(inspector_plugin.editor_selection_changed)
 	add_inspector_plugin(inspector_plugin)
 
 func _exit_tree():
